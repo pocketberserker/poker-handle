185928e8074c6fcc1d69cae9ab50a5bc95293cce
pocketberserker <pocketberserker@gmail.com>
2022-03-17 07:42:01 +0900

    wordle like appbar color

--- src/client/molecules/AppBar/index.tsx
+++ src/client/molecules/AppBar/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const AppBar: React.FC<Props> = ({ appName }) => {
   return (
-    <BaseAppBar position="relative">
+    <BaseAppBar position="relative" color="inherit" elevation={1}>
       <CustomToolbar>
         <Box sx={{ flexGrow: 1 }} />
         <Typography variant="h5">{appName}</Typography>
