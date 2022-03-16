d12a54e16599e50c99af12e5436da698fe238574
pocketberserker <pocketberserker@gmail.com>
2022-03-16 23:18:39 +0900

    centering title

--- src/client/organisms/AppBar/index.tsx
+++ src/client/organisms/AppBar/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { AppBar as BaseAppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar as BaseAppBar, Box, Toolbar, Typography } from "@mui/material";
 import { appName } from "../../constants/meta";
 
 type Props = {};
@@ -9,7 +9,9 @@ export const AppBar: React.FC<Props> = () => {
   return (
     <BaseAppBar position="relative">
       <CustomToolbar>
+        <Box sx={{ flexGrow: 1 }} />
         <Typography variant="h5">{appName}</Typography>
+        <Box sx={{ flexGrow: 1 }} />
       </CustomToolbar>
     </BaseAppBar>
   );
