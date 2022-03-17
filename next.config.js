4c4caa95146e6b93666e7f4d5f02a9b22a92208f
pocketberserker <pocketberserker@gmail.com>
2022-03-17 22:10:31 +0900

    deploy gh-pages (for test)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/poker-handle" : "",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
