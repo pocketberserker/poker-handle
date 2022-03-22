ef9155b329e8ec2c69821192e034833a5fa8a0cb
pocketberserker <pocketberserker@users.noreply.github.com>
2022-03-22 06:34:05 +0000

    deploy: 939bd5812da30e2656cbef191620026fa4ea7d92

--- _next/static/xb1ae_8Q9-2prmCnUTLva/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/921-771ad08620e872c6.js","static/chunks/pages/index-f11e4a05ba7639de.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
