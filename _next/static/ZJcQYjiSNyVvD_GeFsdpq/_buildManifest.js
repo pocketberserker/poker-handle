ca0afe2bf0ef0e31ab24feba58516a092bfcba0f
pocketberserker <pocketberserker@users.noreply.github.com>
2022-03-29 09:22:41 +0000

    deploy: 23fcf526b194a4ead3f2315dc16863b5a5c235a2

---
+++ _next/static/ZJcQYjiSNyVvD_GeFsdpq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/294-fb4b212d1c72ac09.js","static/chunks/pages/index-5953c22d3ca34ce3.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/_Fx_L0NHJcdyrj5iLnAVt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/494-be270fd3b0c41037.js","static/chunks/pages/index-54c816215028eab0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
